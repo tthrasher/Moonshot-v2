c436d3bf0f47815fecf2db0b6524170f795bdc1f
tthrasher <tthrasher@gmail.com>
2022-01-02 19:25:46 -0500

    Introducing GeometryReader

--- Moonshot v2/ContentView.swift
+++ Moonshot v2/ContentView.swift
@@ -1,6 +1,7 @@
 //
 //  ContentView.swift
 //  Moonshot v2
+//  New version is because the lesson has changed + I had stopped in the middle of the project. Starting over seemed ideal!
 //
 //  Created by Terry Thrasher on 2022-01-02.
 //
@@ -9,8 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            Image("landscape_4")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geo.size.width * 0.8, height: 300)
+        }
     }
 }
 
