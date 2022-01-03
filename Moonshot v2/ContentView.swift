8635959a1ca8b5b1e816411c17f4520718d78c01
tthrasher <tthrasher@gmail.com>
2022-01-02 19:11:20 -0500

    Initial Commit

---
+++ Moonshot v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot v2
+//
+//  Created by Terry Thrasher on 2022-01-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot v2/Moonshot_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_v2App.swift
+//  Moonshot v2
+//
+//  Created by Terry Thrasher on 2022-01-02.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
