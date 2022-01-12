631590cecbc7221eec772efa25372459b0d015b5
tthrasher <tthrasher@gmail.com>
2022-01-11 23:44:15 -0500

    NavigationLink introduction

--- Moonshot v2/ContentView.swift
+++ Moonshot v2/ContentView.swift
@@ -8,29 +8,16 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
-                }
+        NavigationView {
+            NavigationLink {
+                Text("Detail View")
+            } label: {
+                Text("Hello world!")
+                    .padding()
             }
-            .frame(maxWidth: .infinity)
+            .navigationTitle("SwiftUI")
         }
     }
 }
