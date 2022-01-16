500478e815f58d13eefc590f6b494ef323f4de14
tthrasher <tthrasher@gmail.com>
2022-01-16 18:08:08 -0500

    Added button and sample JSON and appropriate structs to demonstrate use of Codable

--- Moonshot v2/ContentView.swift
+++ Moonshot v2/ContentView.swift
@@ -8,9 +8,36 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct ContentView: View {
     var body: some View {
         NavigationView {
+            Button("Decode JSON") {
+                let input = """
+                {
+                    "name": "Taylor Swift",
+                    "address": {
+                        "street": "555 Awesome Street",
+                        "city": "Nashville"
+                    }
+                }
+                """
+                
+                let data = Data(input.utf8)
+                let decoder = JSONDecoder()
+                if let user = try? decoder.decode(User.self, from: data) {
+                    print(user.address.street)
+                }
+            }
             List(0..<100) { row in
                 NavigationLink {
                     Text("Detail \(row)")
