77bab9e71959195bcc115913ef244a28a4746767
Terry Thrasher <68034208+tthrasher@users.noreply.github.com>
2022-02-17 18:50:03 -0500

    Added Astronaut file

---
+++ Moonshot v2/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot v2
+//
+//  Created by Terry Thrasher on 2022-02-17.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
